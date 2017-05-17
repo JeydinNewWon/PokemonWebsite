be90235c63756590c0a09556bb5eae02fd17e5a3
Jayden Nguyen <jaydennguyen6669@gmail.com>
2017-05-17 20:38:06 +1000

    a

--- functions.js
+++ functions.js
@@ -3,14 +3,14 @@ var side = function() {
   $('.hamburger').show();
   $('.x-icon').hide();
   $('.hamburger').on('click', function() {
-    $(this).hide(400);
+    $(this).hide(200);
     $('.x-icon').show(100);
     $('.side').fadeIn(300);
   });
   $('.x-icon').on('click', function() {
-    $(this).hide(400);
-    $('.hamburger').show(400);
-    $('.side').fadeOut(380);
+    $(this).hide(300);
+    $('.hamburger').show(200);
+    $('.side').fadeOut(150);
   });
 }
 
